a389c68f5d58b35d6d35b78110a82f072b99e211
Amar <amarkumarreddynew@gmail.com>
2023-03-19 16:24:38 +0530

    initial stage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Navbar from "./components/Navbar/navbar"
+import MyPhoto from "./assets/IMG_6512.JPG"
+
+
+const App = () =>(
+  <div className="react-app">
+    <div className="portfolio-container">
+      <Navbar/>
+      <img src={MyPhoto} alt="amar" className="my-photo"/>
+      <div>
+        <h1>Hai !</h1>
+        <h1>I am Amar kumar Reddy</h1>
+      </div>
+      <div className="details-container">
+          <p>A civil Engineer change his career to a software developer</p>
+          <p>Frontend Developer</p>
+          <p>Backend Developer</p>
+          <button type="button" className="resume-download">Download Resume</button>
+      </div>
     </div>
-  );
-}
+  </div>
+)
 
-export default App;
+export default App
---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,15 @@
+import "./navbar.css"
+
+const Navbar = () =>(
+    <nav classname="navbar-container">
+        <ul classname="nav-list">
+            <li classname="nav-links">About</li>
+            <li classname="nav-links">Skills</li>
+            <li classname="nav-links">Projects</li>
+            <li classname="nav-links">Skills</li>
+        </ul>
+    </nav>
+
+)
+
+export default Navbar
